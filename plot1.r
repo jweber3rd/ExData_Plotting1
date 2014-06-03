31a0c67c34dcfb9881e5c678a0389fbf67dad693
jweber3rd <johnjweber3rd@gmail.com>
2014-06-03 13:11:24 -0400

    R code for corresponding plots for Coursera course "Exploratory Data Analysis".

--- plot1.r
+++ plot1.r
@@ -1,5 +1,11 @@
-# Read table text file as table and store into variable
-hpc<-read.table("household_power_consumption.txt",sep=";",header=TRUE)
+# Read text file and store into variable
+df<-read.csv("household_power_consumption.txt",sep=";",header=TRUE)
+
+# Convert date and time
+my_date = as.POSIXlt(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+
+# Subset dataframe to include only needed dates
+hpc<-subset(df,as.Date(my_date)>=as.Date("2007-02-01") & as.Date(my_date)<=as.Date("2007-02-02"))
 
 # Convert and save Global_active_power as numeric values
 gap<-as.numeric(hpc$Global_active_power)
--- plot2.r
+++ plot2.r
@@ -1,11 +1,14 @@
-# Read table text file as table and store into variable
-hpc<-read.table("household_power_consumption.txt",sep=";",header=TRUE)
+# Read text file and store into variable
+df<-read.csv("household_power_consumption.txt",sep=";",header=TRUE)
+
+# Convert date and time
+my_date = as.POSIXlt(paste(df$Date, df$Time), format="%d/%m/%Y %H:%M:%S")
+
+# Subset dataframe to include only needed dates
+hpc<-subset(df,as.Date(my_date)>=as.Date("2007-02-01") & as.Date(my_date)<=as.Date("2007-02-02"))
 
 # Convert and save Global_active_power as numeric values
 gap<-as.numeric(hpc$Global_active_power)
-
-# Convert date and time
-my_date = as.POSIXlt(paste(hpc$Date, hpc$Time), format="%d/%m/%Y %H:%M:%S")
 
 # Convert date and time to numeric values
 my_date<-as.numeric(my_date)
